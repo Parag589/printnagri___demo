1327ef323a5c0c3690203afec50eeb0d576e31dd
Parag Agarwal <95976519+Parag589@users.noreply.github.com>
2023-05-03 21:39:27 +0530

    Add files via upload

---
+++ Printnagri-custom-id/script.js
@@ -0,0 +1,70 @@
+const downloadCharacterSheet = () => {
+    const node = document.getElementById('id-card');
+    html2canvas(node).then(canvas => {
+      // document.body.appendChild(canvas)
+      // var img    = canvas.toDataURL("image/png");
+      // document.write('<img src="'+img+'"/>');
+      var link = document.createElement('a');
+      link.download = 'filename.png';
+      link.href = canvas.toDataURL();
+      link.click();
+    });
+  };
+  const bindInputToElement = (inputEl, elementEl) => {
+    inputEl.addEventListener('change', () => {
+      elementEl.textContent = inputEl.value;
+    });
+  };
+  document.getElementById('download-button').addEventListener('click', downloadCharacterSheet);
+  document.querySelector('.id-card__subject-id').textContent = md5('something').slice(0, 8);
+  
+  // Bind name
+  const nameEl = document.getElementById('name');
+  bindInputToElement(nameEl, document.getElementById('id-card-name'));
+  nameEl.addEventListener('change', () => {
+    document.querySelector('.id-card__subject-id').textContent = md5(nameEl.value).slice(0, 8);
+  });
+  
+  // Bind date of birth
+  bindInputToElement(document.getElementById('date-of-birth'), document.getElementById('id-card-date-of-birth'));
+  
+  // Bind gender
+  // bindInputToElement(document.getElementById('gender'), document.getElementById('id-card-gender'));
+  
+  // Bind height
+  bindInputToElement(document.getElementById('height'), document.getElementById('id-card-height'));
+  
+  // Bind weight
+  bindInputToElement(document.getElementById('weight'), document.getElementById('id-card-weight'));
+
+  bindInputToElement(document.getElementById('class'), document.getElementById('id-card-class'));
+
+  bindInputToElement(document.getElementById('fname'), document.getElementById('id-card-fname'));
+
+  bindInputToElement(document.getElementById('contact'), document.getElementById('id-card-contact'));
+
+  bindInputToElement(document.getElementById('address'), document.getElementById('id-card-address'));
+  
+  // Bind mugshot
+  document.getElementById('mugshot').addEventListener('change', function () {
+    if (this.files && this.files[0]) {
+      var FR = new FileReader();
+      FR.onload = function (e) {
+        var img = document.getElementById('id-card-mugshot');
+        img.src = e.target.result;
+      };
+      FR.readAsDataURL(this.files[0]);
+    }
+  });
+
+  // Bind mugshot-2
+  document.getElementById('mugshot-2').addEventListener('change', function () {
+    if (this.files && this.files[0]) {
+      var FR = new FileReader();
+      FR.onload = function (e) {
+        var img = document.getElementById('id-card-mugshot-2');
+        img.src = e.target.result;
+      };
+      FR.readAsDataURL(this.files[0]);
+    }
+  });
